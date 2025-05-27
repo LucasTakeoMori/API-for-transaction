37f2162939e089f807ae73d0026110afb2d764cd
Takeo <morilucasaluno@gmail.com>
2025-05-27 13:01:01 -0300

    fix: adjusting port error

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { env } from './env'
 import { app } from './app'
 
+const port = env.PORT || 3333
+
 app
   .listen({
-    port: env.PORT,
+    port,
   })
   .then(() => {
     console.log('HTTP server running')
